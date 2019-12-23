74c42275b387e71380cd034d7e7a602c4a5dcf7a
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2019-12-23 17:27:42 +0530

    disable group config

--- src/main/java/com/java/hc/HazelConfig.java
+++ src/main/java/com/java/hc/HazelConfig.java
@@ -10,8 +10,8 @@ public class HazelConfig {
     private static Config getConfig() {
         com.hazelcast.config.Config config = new com.hazelcast.config.Config();
         config.setInstanceName("studio-instance-test");
-        config.getGroupConfig().setName("studio-group-test");
-        NetworkConfig network = config.getNetworkConfig();
+        //config.getGroupConfig().setName("studio-group-test");
+       // NetworkConfig network = config.getNetworkConfig();
       //  network.setPort(5701);
        // network.setPortAutoIncrement(false);
      //   JoinConfig join = network.getJoin();

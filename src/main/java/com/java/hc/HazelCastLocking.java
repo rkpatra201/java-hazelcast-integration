8f0fc3bd037923876e41e1394d0dcf22c8c5da5c
rabindra patra <rabindrapatra825@gmail.com>
2019-12-23 18:27:07 +0530

    Update HazelCastLocking.java

--- src/main/java/com/java/hc/HazelCastLocking.java
+++ src/main/java/com/java/hc/HazelCastLocking.java
@@ -1,4 +1,27 @@
 package com.java.hc;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ILock;
+
 public class HazelCastLocking {
+    public static void main(String[] args) {
+        HazelcastInstance instance = HazelConfig.getHazelCastInstance();
+        ILock lock = instance.getLock("my-lock");
+        lock.lock();
+        int i =0;
+        while(true)
+        {
+            System.out.println("printing");
+            if(i==1000)
+            {
+                break;
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        lock.unlock();
+    }
 }
